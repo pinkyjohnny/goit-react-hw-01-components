7191cb1aabc8f5b84426baf6b1eb15e2039f2f72
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-10-23 22:52:28 +0300

    stats-css

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,11 +1,16 @@
+import css from './Statistics.module.css'
+const backgroundColor = document.querySelector('[listitem]')
+console.log(backgroundColor);
+
+
 export const Statistics = ({ title = "Upload stats", data }) => {
     return <div>
-<section className="statistics">
-  <h2 className="title">{title}</h2>
+<section className={css.statistics}>
+  <h2 className={css.title}>{title}</h2>
 
-    <ul className="stat-list">
+    <ul className={css.statlist}>
                 {data.map(({ id, label, percentage }) => (
-              <li className="item" key={id}>
+                  <li className={css.listitem} key={id}>
    <span className="label">{label}</span>
       <span className="percentage">{percentage}%</span>
     </li>
