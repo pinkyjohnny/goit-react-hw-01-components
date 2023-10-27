299c0253986d2e09d27e1c47b6e1c912fe780f56
pinkyjohnny <136741239+pinkyjohnny@users.noreply.github.com>
2023-10-27 14:57:33 +0300

    Update TransactionHistory.jsx

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,24 +1,24 @@
 import { TableThStyled, TableTrStyled, TransactionsHistory, TransactionsTableBody, TransactionsTableBodyItem, TransactionsTableHead } from "./TransactionHistory.styled"
 
 export const TransactionHistory = ({ transactions }) => {
-    return <div>
-        <TransactionsHistory>
-  <TransactionsTableHead>
-    <TableTrStyled>
-      <TableThStyled>Type</TableThStyled>
-      <TableThStyled>Amount</TableThStyled>
-      <TableThStyled>Currency</TableThStyled>
-    </TableTrStyled>
-     </TransactionsTableHead>
-    <TransactionsTableBody>     
-   {transactions.map(({ id, type, amount, currency }) => (
-    <TableTrStyled key={id}>
-      <TransactionsTableBodyItem>{type}</TransactionsTableBodyItem>
-      <TransactionsTableBodyItem>{amount}</TransactionsTableBodyItem>
-      <TransactionsTableBodyItem>{currency}</TransactionsTableBodyItem>
-    </TableTrStyled>
-))} 
-  </TransactionsTableBody>
-  </TransactionsHistory>
-    </div>
+  return <div>
+    <TransactionsHistory>
+      <TransactionsTableHead>
+        <TableTrStyled>
+          <TableThStyled>Type</TableThStyled>
+          <TableThStyled>Amount</TableThStyled>
+          <TableThStyled>Currency</TableThStyled>
+        </TableTrStyled>
+      </TransactionsTableHead>
+      <TransactionsTableBody>
+        {transactions.map(({ id, type, amount, currency }) => (
+          <TableTrStyled key={id}>
+            <TransactionsTableBodyItem>{type}</TransactionsTableBodyItem>
+            <TransactionsTableBodyItem>{amount}</TransactionsTableBodyItem>
+            <TransactionsTableBodyItem>{currency}</TransactionsTableBodyItem>
+          </TableTrStyled>
+        ))}
+      </TransactionsTableBody>
+    </TransactionsHistory>
+  </div>
 }
